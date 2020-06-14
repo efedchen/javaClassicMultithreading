22758bc794051f83482fbbb312220db60224aba9
eugeniusz <eugeniusz.fed@gmail.com>
2020-06-14 17:05:55 +0300

    Re entrant locks example

---
+++ src/com/home/eugene/reEntrantLocks/Runner.java
@@ -0,0 +1,56 @@
+package com.home.eugene.reEntrantLocks;
+
+import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Runner {
+    private int count = 0;
+
+    private Lock lock = new ReentrantLock();
+    //await and signal - are equivalents to Object methods wait and notify
+    private Condition cond = lock.newCondition();
+
+    private void increment() {
+        for (int i = 0; i < 10000; i++) {
+            count++;
+        }
+    }
+
+    public void firstThread() throws InterruptedException {
+        lock.lock();
+
+        System.out.println("Waiting");
+        cond.await();
+
+        System.out.println("Woken up");
+        try {
+            increment();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void secondThread() throws InterruptedException {
+        Thread.sleep(1000);
+        lock.lock();
+
+        System.out.println("Press the return key!");
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+        System.out.println("Gor return key");
+
+        cond.signal();
+
+        try {
+            increment();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void finished() {
+        System.out.println("Count is: " + count);
+    }
+}
